743bf3cd483bf55bf9a5ad47e90315b592fa14c9
bagasalfian <bagasalfian72@gmail.com>
2020-04-14 15:06:42 +0700

    tampilan update lagi

--- android/app/src/main/java/com/example/enigma/SignUpDosen.java
+++ android/app/src/main/java/com/example/enigma/SignUpDosen.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignUpDosen extends AppCompatActivity {
     private Button buttonsignupdos;
     private Button buttonkelogin;
+    private TextView showpassdosen;
+    private EditText passdosen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,50 @@ public class SignUpDosen extends AppCompatActivity {
                 openKeLogIn();
             }
         });
+
+        passdosen = (EditText) findViewById(R.id.passdosen);
+        showpassdosen = (TextView)findViewById(R.id.showpassdosen);
+
+
+        showpassdosen.setVisibility(View.GONE);
+        passdosen.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        passdosen.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(passdosen.getText().length()>0){
+                    showpassdosen.setVisibility(View.VISIBLE);
+                }
+                else{
+                    showpassdosen.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        showpassdosen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(showpassdosen.getText() == "SHOW"){
+                    passdosen.setText("HIDE");
+                    passdosen.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passdosen.setSelection(passdosen.length());
+                }
+                else{
+                    showpassdosen.setText("SHOW");
+                    passdosen.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    passdosen.setSelection(passdosen.length());
+                }
+            }
+        });
+
     }
 
     public void openSignUpDos(){
--- android/app/src/main/java/com/example/enigma/SignUpMahasiswa.java
+++ android/app/src/main/java/com/example/enigma/SignUpMahasiswa.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignUpMahasiswa extends AppCompatActivity {
     private Button buttondaftarmahasiswa,kembalikelogin;
+    private TextView show;
+    private EditText passmaha;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +34,49 @@ public class SignUpMahasiswa extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openKembaliLogin();
+            }
+        });
+
+        passmaha = (EditText) findViewById(R.id.passmaha);
+        show = (TextView)findViewById(R.id.show);
+
+        show.setVisibility(View.GONE);
+        passmaha.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        passmaha.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(passmaha.getText().length()>0){
+                    show.setVisibility(View.VISIBLE);
+                }
+                else{
+                    show.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(show.getText() == "SHOW"){
+                    show.setText("HIDE");
+                    passmaha.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passmaha.setSelection(passmaha.length());
+                }
+                else{
+                    show.setText("SHOW");
+                    passmaha.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    passmaha.setSelection(passmaha.length());
+                }
             }
         });
     }
