d24b42648e48192c627e3c05e81007e5edc1b473
Dicki Kurniawan <dickikurniawan15@gmail.com>
2020-06-28 16:17:30 +0700

    build qr dan input kehadiran

--- database/Enigma/database/Absen.php
+++ database/Enigma/database/Absen.php
@@ -4,14 +4,16 @@
 	$nip = $_POST['nip'];
 	$nim = $_POST['nim'];
 
-    $Sql_Query = "SELECT id_mhs FROM mahasiswa WHERE email = '$nim'";
-    $idMaha= mysqli_query($con,$Sql_Query);
+    // $Sql_Query = "SELECT id_mhs FROM mahasiswa WHERE email = '$nim'";
+    // $idMaha= mysqli_query($con,$Sql_Query);
 
-    $Sql_Query = "SELECT id_dsn FROM WHERE email = '$nip'";
-    $idDsn= mysqli_query($con,$Sql_Query);
+    // $Sql_Query = "SELECT id_dsn FROM WHERE email = '$nip'";
+    // $idDsn= mysqli_query($con,$Sql_Query);
 
+    $sqls = "INSERT INTO session (id_mhs,id_dsn,waktu) VALUES ( '$nim', '$nip',now())";
+	$querys = mysqli_query($con,$sqls);
 
-	$sql = "INSERT INTO kehadiran (id_mhs,id_dsn) VALUES ( '$idMaha' '$idDsn')";
+	$sql = "INSERT INTO kehadiran (id_mhs,id_dsn,waktu) VALUES ( '$nim', '$nip',now())";
 	$query = mysqli_query($con,$sql);
 
 	if($query){
