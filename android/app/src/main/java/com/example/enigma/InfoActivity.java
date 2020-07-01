12e1d0cf28de19ca30549ecc957a8460d00f4e30
bagasalfian <bagasalfian72@gmail.com>
2020-07-01 19:59:40 +0700

    tampilan paling baru

--- android/app/src/main/java/com/example/enigma/InfoActivity.java
+++ android/app/src/main/java/com/example/enigma/InfoActivity.java
@@ -2,13 +2,29 @@ package com.example.enigma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class InfoActivity extends AppCompatActivity {
+    private ImageView openbacktoMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        openbacktoMain = (ImageView) findViewById(R.id.openbacktoMain);
+        openbacktoMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openkeMain();
+            }
+        });
+    }
+    public void openkeMain(){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
--- android/app/src/main/java/com/example/enigma/TeamActivity.java
+++ android/app/src/main/java/com/example/enigma/TeamActivity.java
@@ -2,13 +2,31 @@ package com.example.enigma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class TeamActivity extends AppCompatActivity {
+
+    private ImageView OpenBacktoMainMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team);
+
+        OpenBacktoMainMenu = (ImageView) findViewById(R.id.OpenBacktoMainMenu);
+        OpenBacktoMainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenkeMenuAwal();
+            }
+        });
+    }
+
+    public void OpenkeMenuAwal(){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
